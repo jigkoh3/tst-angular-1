db39163ce112690ea00990cd56ce07974a9a12ca
jigkoh3 <jigkoh3@hotmail.com>
2016-05-11 06:31:15 +0700

    initail

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tst-angular-1
 step 1. test angular stucture
+
+remark : In this project have invalid soucrecode (3 point), Can you help me?
---
+++ app.js
@@ -0,0 +1,4 @@
+var myApp = angular.module("myApp",[]);
+myApp.controller('NameCtrl', function($scope){
+	$scope.name = "theerasak tubrit";
+})
